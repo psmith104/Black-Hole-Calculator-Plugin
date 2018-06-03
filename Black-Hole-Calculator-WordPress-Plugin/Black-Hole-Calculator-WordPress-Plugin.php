7b8af82c8aaca649922424d254353ddc93947e45
Patrick Smith <psmith104@gmail.com>
2018-06-03 16:02:59 -0400

    Include Isolated Bootstrap when Shortcode is called

--- Black-Hole-Calculator-WordPress-Plugin/Black-Hole-Calculator-WordPress-Plugin.php
+++ Black-Hole-Calculator-WordPress-Plugin/Black-Hole-Calculator-WordPress-Plugin.php
@@ -7,6 +7,7 @@ Description:  Black Hole Calculator developed for Fabio Pacucci by Patrick M Smi
 /* [BlackHoleCalculator] */
 function ShortcodeOutput($atts)
 {
+	wp_enqueue_style("IsolatedBootstrap", plugins_url('bootstrap.iso.css', __FILE__), array(), false, "all");
 	return file_get_contents(plugins_url('Calculator.html', __FILE__));
 }
 add_shortcode('BlackHoleCalculator', 'ShortcodeOutput');
