2139fc56eff3bcb0bd32dd373b7b806eb5a7feeb
Patrick Smith <psmith104@gmail.com>
2018-06-03 14:46:22 -0400

    Add Shortcode Ouput

--- Black-Hole-Calculator-WordPress-Plugin/Black-Hole-Calculator-WordPress-Plugin.php
+++ Black-Hole-Calculator-WordPress-Plugin/Black-Hole-Calculator-WordPress-Plugin.php
@@ -3,4 +3,11 @@
 Plugin Name:  Black Hole Calculator
 Description:  Black Hole Calculator developed for Fabio Pacucci by Patrick M Smith
 */
+
+/* [BlackHoleCalculator] */
+function ShortcodeOutput($atts)
+{
+	return file_get_contents(plugins_url('Calculator.html', __FILE__));
+}
+add_shortcode('BlackHoleCalculator', 'ShortcodeOutput');
 ?>
