5be87bbbc99b7326d16b8a4e8453b225c0e47ba7
Patrick Smith <psmith104@gmail.com>
2018-06-03 18:23:37 -0400

    Toggle advanced with radio click

---
+++ Black-Hole-Calculator-WordPress-Plugin/Calculator.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $("#simple").on("click", function () {
+        $("[data-advanced]").data("advanced", false);
+    });
+
+    $("#advanced").on("click", function () {
+        $("[data-advanced]").data("advanced", true);
+    });
+}) (jQuery)
