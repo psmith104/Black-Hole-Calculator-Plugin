ab6c9892599446779b505b5246c2609201858602
Patrick Smith <psmith104@gmail.com>
2018-06-03 14:41:37 -0400

    Add initial plugin files

---
+++ Black-Hole-Calculator-WordPress-Plugin/Black-Hole-Calculator-WordPress-Plugin.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Plugin Name:  Black Hole Calculator
+Description:  Black Hole Calculator developed for Fabio Pacucci by Patrick M Smith
+*/
+?>
---
+++ Black-Hole-Calculator-WordPress-Plugin/readme.txt
@@ -0,0 +1,3 @@
+=== Black Hole Calculator WordPress Plugin ===
+
+Black Hole Calculator WordPress plugin developed for Fabio Pacucci by Patrick Smith.
