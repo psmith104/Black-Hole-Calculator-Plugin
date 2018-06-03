7bbbdd7b1321067bfafb6fd87b255b2b1deee76b
Patrick Smith <psmith104@gmail.com>
2018-06-03 16:59:36 -0400

    Add Boostrap overide so inputs do not get stretched

--- Black-Hole-Calculator-WordPress-Plugin/Black-Hole-Calculator-WordPress-Plugin.php
+++ Black-Hole-Calculator-WordPress-Plugin/Black-Hole-Calculator-WordPress-Plugin.php
@@ -8,6 +8,7 @@ Description:  Black Hole Calculator developed for Fabio Pacucci by Patrick M Smi
 function ShortcodeOutput($atts)
 {
 	wp_enqueue_style("IsolatedBootstrap", plugins_url('bootstrap/bootstrap.iso.css', __FILE__), array(), false, "all");
+	wp_enqueue_style("BlackHoleCalculator", plugins_url('Calculator.css', __FILE__), array(), false, "all");
 	wp_enqueue_script("jQuery");
 	wp_enqueue_script("IsolatedBootstrap", plugins_url('bootstrap/bootstrap.min.js', __FILE__), array(), false, true);
 	return file_get_contents(plugins_url('Calculator.html', __FILE__));
