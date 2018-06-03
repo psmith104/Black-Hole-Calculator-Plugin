9c0089c5a5bd7225035d66d79fbf2b4b52a53b62
Patrick Smith <psmith104@gmail.com>
2018-06-03 18:55:38 -0400

    Add calculation for EddingtonLuminosity

--- Black-Hole-Calculator-WordPress-Plugin/Calculator.js
+++ Black-Hole-Calculator-WordPress-Plugin/Calculator.js
@@ -1,11 +1,30 @@
 (function ($) {
     $(function () {
-        $("#standard").on("change", function () {
+        $("#standard").on("change", function ()
+        {
             $("[data-advanced]").attr("data-advanced", false);
         });
 
-        $("#advanced").on("change", function () {
+        $("#advanced").on("change", function ()
+        {
             $("[data-advanced]").attr("data-advanced", true);
         });
+
+        $("calculate-button").on("click", function ()
+        {
+            var mbh = $("#blackHoleMass").val();
+            var mSeed = $("[data-advanced]").attr("data-advanced") ? $("#massOfSeed").val() : null;
+
+            var results = PerformCalculations(mbh, mSeed);
+
+            $("#eddingtonLuminosity").html(results.eddingtonLuminosity);
+        });
+
+        function PerformCalculations(bhm, mSeed)
+        {
+            return {
+                eddingtonLuminosity = 3.2e4 * mbh
+            };
+        }
     });
 }) (jQuery)
