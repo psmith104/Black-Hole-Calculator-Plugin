9d18a409bd965e2b88667c2bab86dcd42a01a887
Patrick Smith <psmith104@gmail.com>
2018-06-03 16:33:14 -0400

    Add bootstrap.min.js and move all bootstrap into subdirectory

--- Black-Hole-Calculator-WordPress-Plugin/Black-Hole-Calculator-WordPress-Plugin.php
+++ Black-Hole-Calculator-WordPress-Plugin/Black-Hole-Calculator-WordPress-Plugin.php
@@ -7,7 +7,9 @@ Description:  Black Hole Calculator developed for Fabio Pacucci by Patrick M Smi
 /* [BlackHoleCalculator] */
 function ShortcodeOutput($atts)
 {
-	wp_enqueue_style("IsolatedBootstrap", plugins_url('bootstrap.iso.css', __FILE__), array(), false, "all");
+	wp_enqueue_style("IsolatedBootstrap", plugins_url('bootstrap/bootstrap.iso.css', __FILE__), array(), false, "all");
+	wp_enqueue_script("jQuery");
+	wp_enqueue_script("IsolatedBootstrap", plugins_url('bootstrap/bootstrap.min.js', __FILE__), array(), false, true);
 	return file_get_contents(plugins_url('Calculator.html', __FILE__));
 }
 add_shortcode('BlackHoleCalculator', 'ShortcodeOutput');
